4c3cf58c39bdb2e114fa93a0f50e7a7652922ada
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-26 18:26:23 +0100

    Initial commit

---
+++ src/main/java/com/homemanagment/homemanagment/HomeManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.homemanagment.homemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homemanagment/homemanagment/HomeManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homemanagment.homemanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
